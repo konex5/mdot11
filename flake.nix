6453391bfd4920c0997d4d0e8da791c29d769104
konex5 <fix@konex.dev>
2025-02-21 16:13:18 +0100

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -30,29 +30,29 @@
         let
           inherit (prev.lib) composeExtensions;
           pythonPackageOverrides = python-self: python-super: {
-            #     inherit (python-self.callPackage ./derivation.nix {
-            #     src = self;
-            #     stdenv = if prev.stdenv.hostPlatform.isDarwin then final.clangStdenv else final.gccStdenv;
-            # }) pyfhmdot;
+            inherit (with python-self; callPackage ./derivation.nix {
+              src = self;
+              stdenv = if prev.stdenv.hostPlatform.isDarwin then final.clangStdenv else final.gccStdenv;
+            }) pyfhmdot;
           };
         in
         {
-          # python37 = prev.python37.override (old: {
-          #     packageOverrides =
-          #       composeExtensions (old.packageOverrides or (_: _: { }))
-          #         pythonPackageOverrides;
-          #   });
-          #   python38 = prev.python38.override (old: {
-          #     packageOverrides =
-          #       composeExtensions (old.packageOverrides or (_: _: { }))
-          #         pythonPackageOverrides;
-          #   });
-          #   python39 = prev.python39.override (old: {
-          #     packageOverrides =
-          #       composeExtensions (old.packageOverrides or (_: _: { }))
-          #         pythonPackageOverrides;
-          #   });
-          #   python3 = final.python39;
+          python37 = prev.python37.override (old: {
+            packageOverrides =
+              composeExtensions (old.packageOverrides or (_: _: { }))
+                pythonPackageOverrides;
+          });
+          python38 = prev.python38.override (old: {
+            packageOverrides =
+              composeExtensions (old.packageOverrides or (_: _: { }))
+                pythonPackageOverrides;
+          });
+          python39 = prev.python39.override (old: {
+            packageOverrides =
+              composeExtensions (old.packageOverrides or (_: _: { }))
+                pythonPackageOverrides;
+          });
+          python3 = final.python39;
 
           inherit (prev.callPackage ./derivation.nix {
             src = self;
@@ -64,6 +64,9 @@
           }).libfhmdot;
         };
 
+      defaultPackage = forAllSystems (system:
+        self.packages.${system}.libfhmdot);
+
       devShell = forDevSystems (system:
         let pkgs = nixpkgsFor.${system}; in pkgs.callPackage ./shell.nix { }
       );
@@ -71,7 +74,7 @@
       hydraJobs = {
         build = forDevSystems (system: nixpkgsFor.${system}.libfhmdot);
         build-clang = forDevSystems (system: nixpkgsFor.${system}.libfhmdot-clang);
-        # build-python3 = forDevSystems (system: nixpkgsFor.${system}.python3Packages.pyfhmdot );
+        build-python3 = forDevSystems (system: nixpkgsFor.${system}.python3Packages.pyfhmdot);
 
         release = forDevSystems (system:
           with nixpkgsFor.${system}; releaseTools.aggregate
@@ -80,21 +83,19 @@
               constituents =
                 [
                   self.hydraJobs.build-clang.${system}
-                  # self.hydraJobs.build-python3
+                  self.hydraJobs.build-python3
                 ] ++ lib.optionals (hostPlatform.isLinux) [
                   self.hydraJobs.build.${system}
                 ];
               meta.description = "hydraJobs: ${repoDescription}";
             });
       };
+
       packages = forAllSystems (system:
         with nixpkgsFor.${system}; {
           inherit libfhmdot libfhmdot-clang;
-          # inherit (python3Packages) pyfhmdot;
+          inherit (python3Packages) pyfhmdot;
         });
-
-      defaultPackage = forAllSystems (system:
-        self.packages.${system}.libfhmdot);
 
       # apps = forAllSystems (system: {
       #   fhmdot = {
