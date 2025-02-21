e9c0165ccc6244a5f1dd2094c7e598da66c6fb64
konex5 <fix@konex.dev>
2025-02-21 16:13:19 +0100

    first working test but the GIL mechanism

--- src/pybind/mdot_routine.cpp
+++ src/pybind/mdot_routine.cpp
@@ -6,7 +6,6 @@
 #include "mdot/include/routine/interface.hpp"
 #include "pybind/include/translate.hpp"
 
-#include <iostream>
 namespace py = pybind11;
 
 template <typename T> using numpy_array = py::array_t<T, py::array::c_style>;
@@ -17,7 +16,7 @@ using pydmbloc_type =
 using pydtbloc_type = std::map<std::tuple<uint16_t, uint8_t, uint8_t, uint16_t>,
                                numpy_array<dnum_t>>;
 
-void py_mm_to_theta_no_gate(pydtbloc_type dst, pydmbloc_type lhs,
+pydtbloc_type py_mm_to_theta_no_gate(pydmbloc_type lhs,
                             pydmbloc_type rhs,
                             bool conserve_left_right = false) {
   dmbloc_t tmp_lhs, tmp_rhs;
@@ -25,14 +24,15 @@ void py_mm_to_theta_no_gate(pydtbloc_type dst, pydmbloc_type lhs,
   translate_dmbloc_py2cpp(tmp_rhs, rhs);
   dtbloc_t tmp_dst;
   mdot::mm_to_theta_no_gate(tmp_dst, tmp_lhs, tmp_rhs, conserve_left_right);
+  pydtbloc_type dst;
   translate_dtbloc_cpp2py(dst, tmp_dst);
-  std::cout << "metal!" << std::endl;
+  return dst;
 }
 
 PYBIND11_MODULE(mdot_routine, m) {
   m.doc() = "routine to speedup code execution";
 
-  m.def("mm_to_theta_no_gate", &py_mm_to_theta_no_gate, py::arg("dst_blocs"),
+  m.def("mm_to_theta_no_gate", &py_mm_to_theta_no_gate,
         py::arg("lhs_blocs"), py::arg("rhs_blocs"),
         py::arg("conserve_left_right") = false, "execute mm_to_theta_no_gate.");
 }
--- tests/python_unit/test_mdot_routine.py
+++ tests/python_unit/test_mdot_routine.py
@@ -7,5 +7,16 @@ def test_mdot_routine_mm_to_theta_no_gate(make_random_blocs):
     import mdot_routine
 
     dst = {}
-    mdot_routine.mm_to_theta_no_gate(dst, mps_left, mps_right, True)
+    a = mdot_routine.mm_to_theta_no_gate(mps_left, mps_right, True)
+    pass
+
+
+def test_mdot_routine_mm_to_theta_no_gate_compare(make_random_blocs):
+    mps_left = make_random_blocs(20, 4)
+    mps_right = make_random_blocs(20, 4)
+    import mdot_routine
+    from pyfhmdot.routine.interface import mm_to_theta_no_gate
+    dst = {}
+    mm_to_theta_no_gate(dst, mps_left, mps_right, conserve_left_right=True)
+    a = mdot_routine.mm_to_theta_no_gate(mps_left, mps_right, True)
     pass
