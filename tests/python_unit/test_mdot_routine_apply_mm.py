1455c0381a795fb6522196b974df4e511f998ca2
konex5 <fix@konex.dev>
2025-02-21 16:13:20 +0100

    separate test

---
+++ tests/python_unit/test_mdot_routine_apply_mm.py
@@ -0,0 +1,29 @@
+import pytest
+
+def test_mdot_routine_routine(make_random_blocs):
+    mps_left = make_random_blocs(20, 2)
+    mps_right = make_random_blocs(20, 1)
+    import mdot_routine
+    from pyfhmdot.algorithm import apply_mm
+    from pyfhmdot.intense.mul_mp import multiply_mp
+    from copy import deepcopy
+
+    dw_dict = {"dw_one_serie": 0}
+    m_left, m_right = apply_mm(
+        deepcopy(mps_left),
+        deepcopy(mps_right),
+        dw_dict,
+        8,
+        True,
+        10**-8,
+        is_um=True,
+        conserve_left_right_before=False,
+        direction_right=1,
+    )
+    left, right, dw = mdot_routine.apply_mm(
+        deepcopy(mps_left), deepcopy(mps_right), 8, True, 10**-8, 1, 1
+    )
+    m_dst = {}
+    multiply_mp(m_dst, m_left, m_right, [2], [0])
+    dst = {}
+    multiply_mp(dst, left, right, [2], [0])
--- tests/python_unit/test_mdot_routine_theta_no_gate.py
+++ tests/python_unit/test_mdot_routine_theta_no_gate.py
@@ -26,33 +26,3 @@ def test_mdot_routine_mm_to_theta_no_gate_compare(make_random_blocs):
     cpp_dst = cpp_apply(deepcopy(mps_left), deepcopy(mps_right), False)
     for key in dst.keys():
         assert all(dst[key] == cpp_dst[key])
-
-
-def test_mdot_routine_routine(make_random_blocs):
-    mps_left = make_random_blocs(20, 2)
-    mps_right = make_random_blocs(20, 1)
-    import mdot_routine
-    from pyfhmdot.algorithm import apply_mm
-    from pyfhmdot.intense.mul_mp import multiply_mp
-    from copy import deepcopy
-
-    dw_dict = {"dw_one_serie": 0}
-    m_left, m_right = apply_mm(
-        deepcopy(mps_left),
-        deepcopy(mps_right),
-        dw_dict,
-        8,
-        True,
-        10**-8,
-        is_um=True,
-        conserve_left_right_before=False,
-        direction_right=1,
-    )
-    left, right, dw = mdot_routine.apply_mm(
-        deepcopy(mps_left), deepcopy(mps_right), 8, True, 10**-8, 1, 1
-    )
-    m_dst = {}
-    multiply_mp(m_dst, m_left, m_right, [2], [0])
-    dst = {}
-    multiply_mp(dst, left, right, [2], [0])
-    pass
