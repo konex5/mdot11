1bf137611dc98cc760ffd655e4a60e02c683b609
konex5 <fix@konex.dev>
2025-02-21 16:13:20 +0100

    disable some mdot routine

--- src/pybind/mdot_routine.cpp
+++ src/pybind/mdot_routine.cpp
@@ -18,7 +18,7 @@ using pydgbloc_type = std::map<std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>,
 
 using pydtbloc_type = std::map<std::tuple<uint16_t, uint8_t, uint8_t, uint16_t>,
                                numpy_array<dnum_t>>;
-
+/*
 pydtbloc_type py_mm_to_theta_no_gate(pydmbloc_type lhs, pydmbloc_type rhs,
                                      bool conserve_left_right = false) {
   dmbloc_t tmp_lhs, tmp_rhs;
@@ -47,7 +47,7 @@ pydtbloc_type py_mm_to_theta_with_gate(pydmbloc_type lhs, pydmbloc_type rhs,
   translate_dtbloc_cpp2py(dst_out, tmp_dst);
   return dst_out;
 }
-
+*/
 std::tuple<pydmbloc_type, pydmbloc_type, dnum_t>
 py_apply_mm(pydmbloc_type lhs, pydmbloc_type rhs, const index_t chi_max,
             const dnum_t eps, const bool normalize, const int is_um,
@@ -70,15 +70,45 @@ py_apply_mm(pydmbloc_type lhs, pydmbloc_type rhs, const index_t chi_max,
   return dst_out;
 }
 
+
+std::tuple<pydmbloc_type, pydmbloc_type, dnum_t>
+py_apply_gate_on_mm(pydmbloc_type lhs, pydmbloc_type rhs, const pydgbloc_type gate, const index_t chi_max,
+            const dnum_t eps, const bool normalize, const int is_um,
+            const int direction_right) {
+  dmbloc_t lhs_in, rhs_in;
+  dgbloc_t gate_in;
+  translate_dmbloc_py2cpp(lhs_in, lhs);
+  translate_dmbloc_py2cpp(rhs_in, rhs);
+  translate_dgbloc_py2cpp(gate_in, gate);
+  dtbloc_t tmp_dst;
+  mdot::mm_to_theta_with_gate(tmp_dst, lhs_in, rhs_in, gate_in, false);
+  lhs_in.clear();
+  rhs_in.clear();
+  dnum_t dw = 0;
+  mdot::theta_to_mm(tmp_dst, lhs_in, rhs_in, dw, chi_max, normalize, is_um,
+                    direction_right, eps);
+  tmp_dst.clear();
+  std::tuple<pydmbloc_type, pydmbloc_type, dnum_t> dst_out;
+  translate_dmbloc_cpp2py(std::get<0>(dst_out), lhs_in);
+  translate_dmbloc_cpp2py(std::get<1>(dst_out), rhs_in);
+  std::get<2>(dst_out) = dw;
+  return dst_out;
+}
+
 PYBIND11_MODULE(mdot_routine, m) {
   m.doc() = "routine to speedup code execution";
   m.def("apply_mm", &py_apply_mm, py::arg("mps_left"), py::arg("mps_right"),
         py::arg("chi_max"), py::arg("eps"), py::arg("normalize"),
         py::arg("is_um"), py::arg("direction_right"));
+  m.def("apply_gate_on_mm", &py_apply_gate_on_mm, py::arg("mps_left"), py::arg("mps_right"), py::arg("gate"),
+        py::arg("chi_max"), py::arg("eps"), py::arg("normalize"),
+        py::arg("is_um"), py::arg("direction_right"));
+  /*
   m.def("mm_to_theta_no_gate", &py_mm_to_theta_no_gate, py::arg("lhs_blocs"),
         py::arg("rhs_blocs"), py::arg("conserve_left_right") = false,
         "execute mm_to_theta_no_gate.");
   m.def("mm_to_theta_with_gate", &py_mm_to_theta_with_gate,
         py::arg("lhs_blocs"), py::arg("rhs_blocs"), py::arg("gate_blocs"),
         py::arg("conserve_left_right") = false, "execute mm_to_theta_no_gate.");
+  */
 }
--- tests/python_unit/test_mdot_routine_apply_mm.py
+++ tests/python_unit/test_mdot_routine_apply_mm.py
@@ -1,41 +1,23 @@
 from copy import deepcopy
 import pytest
 
+
 def test_mdot_routine_apply_mm_empty():
     import mdot_routine
-    
-    mdot_routine.apply_mm({},{},8,10**-8,True,1,1)
+
+    mdot_routine.apply_mm({}, {}, 8, 10**-8, True, 1, 1)
+
 
 def test_mdot_routine_apply_mm_minimal(make_random_blocs):
     import mdot_routine
     from pyfhmdot.algorithm import apply_mm
     import numpy as np
 
-    a = make_random_blocs(1,1)
-    dw_dict = {"dw_one_serie":0}
-    left,right = apply_mm(deepcopy(a),deepcopy(a),dw_dict,8,True,10**-8,is_um=True,conserve_left_right_before=False,direction_right=1)
-    m_left, m_right, dw = mdot_routine.apply_mm(deepcopy(a),deepcopy(a),8,10**-8,True,1,1)
-    #assert dw <= dw_dict["dw_one_serie"]
-    for key in left.keys():
-        assert np.all(np.abs(np.abs(left[key]) - np.abs(m_left[key])) < 10**-8)
-    for key in right.keys():
-        assert np.all(np.abs(np.abs(right[key]) - np.abs(right[key])) < 10**-8)
-        
-
-
-def test_mdot_routine_routine(make_random_blocs):
-    mps_left = make_random_blocs(20, 2)
-    mps_right = make_random_blocs(20, 3)
-    import mdot_routine
-    from pyfhmdot.algorithm import apply_mm
-    from pyfhmdot.intense.mul_mp import multiply_mp
-    from copy import deepcopy
-    #import numpy as np
-
+    a = make_random_blocs(1, 1)
     dw_dict = {"dw_one_serie": 0}
-    m_left, m_right = apply_mm(
-        deepcopy(mps_left),
-        deepcopy(mps_right),
+    left, right = apply_mm(
+        deepcopy(a),
+        deepcopy(a),
         dw_dict,
         8,
         True,
@@ -44,23 +26,50 @@ def test_mdot_routine_routine(make_random_blocs):
         conserve_left_right_before=False,
         direction_right=1,
     )
-    left, right, dw = mdot_routine.apply_mm(
-        deepcopy(mps_left), deepcopy(mps_right), 8, 10**-8, True, 1, 1
+    m_left, m_right, dw = mdot_routine.apply_mm(
+        deepcopy(a), deepcopy(a), 8, 10**-8, True, 1, 1
     )
-    # assert dw_dict["dw_one_serie"] == dw
-    # for key in m_left.keys():
-    #     m_left[key].transpose() == left[key]
-    # for key in m_right.keys():
-    #     m_right[key] == right[key]
+    assert abs(dw - dw_dict["dw_one_serie"]) < 10**-8
+    for key in left.keys():
+        assert np.all(np.abs(np.abs(left[key]) - np.abs(m_left[key])) < 10**-8)
+    for key in right.keys():
+        assert np.all(np.abs(np.abs(right[key]) - np.abs(right[key])) < 10**-8)
+
+
+def test_mdot_routine_routine(make_random_blocs):
+    mps_left = make_random_blocs(20, 1)
+    mps_right = make_random_blocs(20, 1)
+    import mdot_routine
+    from pyfhmdot.algorithm import apply_mm
+    from pyfhmdot.intense.mul_mp import multiply_mp
+    from copy import deepcopy
+
+    import numpy as np
+
+    dw_dict = {"dw_one_serie": 0}
+    m_left, m_right = apply_mm(
+        deepcopy(mps_left),
+        deepcopy(mps_right),
+        dw_dict,
+        8,
+        False,
+        10**-62,
+        is_um=True,
+        conserve_left_right_before=False,
+        direction_right=1,
+    )
+    left, right, dw = mdot_routine.apply_mm(
+        deepcopy(mps_left), deepcopy(mps_right), 8, 10**-62, False, 1, 1
+    )
+    assert abs(dw - dw_dict["dw_one_serie"]) < 10**-8
+    for key in left.keys():
+        np.all(np.abs(np.abs(left[key]) - np.abs(m_left[key])) < 10**-8)
+    for key in right.keys():
+        np.all(right[key] == m_right[key])
     
-    # for key in left.keys():
-    #     assert np.all(np.abs(np.abs(left[key]) - np.abs(m_left[key])) < 10**-8)
-    # for key in right.keys():
-    #     assert np.all(np.abs(np.abs(right[key]) - np.abs(right[key])) < 10**-8)
-        
-
-
     m_dst = {}
     multiply_mp(m_dst, m_left, m_right, [2], [0])
     dst = {}
     multiply_mp(dst, left, right, [2], [0])
+    for key in m_dst.keys():
+        np.all(np.abs(m_dst[key] - dst[key])<10**-8)
