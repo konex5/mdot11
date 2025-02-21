f5cb42c53f0a280301ef7f628735cc0642f7a51f
konex5 <fix@konex.dev>
2025-02-21 16:13:19 +0100

    add b

--- src/pybind/operators_mdot.cpp
+++ src/pybind/operators_mdot.cpp
@@ -1,8 +1,9 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
 #include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+//#include <pybind11/stl_bind.h>
 
-
+#include <iostream>
 #include <string>
 #include <map>
 #include <tuple>
@@ -28,16 +29,24 @@ pydblocs_type single_operator_re(std::string name, std::string qbasis) {
     auto blocs = std::get<0>(test);
     //
     pydblocs_type output;
-    std::tuple<uint8_t, uint8_t> indices;
-    std::vector<dnum_t> vec_arr;
     //
-    
     for (auto const& x : blocs) {
-        indices = {std::get<0>(x.first),std::get<1>(x.first)};
-        vec_arr = x.second;
+        std::tuple<uint8_t, uint8_t> indices = {std::get<0>(x.first),std::get<1>(x.first)};
+        std::vector<dnum_t> vec_arr = x.second;
+        //
         std::size_t n = vec_arr.size();
-        auto ptr = vec_arr.data();
-        auto deallocator = py::capsule(&vec_arr, [](void* ptr) { auto vec_ptr = reinterpret_cast<std::vector<dnum_t> *>(ptr); vec_ptr->clear(); });
+        for (int i =0;i<4;i++) {
+            std::cout << vec_arr[i] << " " << std::endl;
+        }
+        //vec_arr.reserve(n);
+        dnum_t* ptr = vec_arr.data();
+        auto deallocator = py::capsule(&vec_arr, [](void* ptr) { 
+            auto vec_ptr = reinterpret_cast<std::vector<dnum_t> *>(ptr);
+            for (int i =0;i<4;i++) {
+                std::cout << vec_ptr->at(i) << " " << std::endl;
+            }
+            vec_ptr->clear(); 
+            });
         numpy_array<dnum_t> np_out(n, ptr, deallocator);
         output[indices] = np_out;
     }
@@ -60,6 +69,7 @@ numpy_array<dnum_t> single_operator_r(std::string name, std::string qbasis) {
 
 PYBIND11_MODULE(operators_mdot, m) {
     m.doc() = "a pybind11 for quantum simulations";
+    //py::bind_map<std::map<std::tuple<uint8_t, uint8_t>, numpy_array<double>>>(m, "MapBlocsDouble");
     m.def("single_operator_r", &single_operator_r, "return operators");
     m.def("single_operator_re", &single_operator_re, "return operators");
 }
