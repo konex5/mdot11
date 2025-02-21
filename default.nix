a6433c13fc2d5e20c68e02b73b541936f912e6f1
konex5 <fix@konex.dev>
2025-02-21 16:13:19 +0100

    cleanup nix files and focus on the wrapper purpose

--- default.nix
+++ default.nix
@@ -1,17 +1,21 @@
-with (import
-  (
-    builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/e387bb3a4d0d63446086abbfbe2117e80fe3522a.tar.gz";
-      sha256 = "sha256:1hhla9k5v0ri76vcaz7pmkwc7kqj45hca3ygfd1c1lsbg01fnvbp";
-    }
-  )
-  {
-    config.allowUnfree = true;
-  });
+{ pkgs ? import
+    (
+      builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/902d91def1efbea804f5158e5999cb113cedf04b.tar.gz";
+        sha256 = "sha256:1ya19ix77k2yn1c2gyzz644576c2qn11llrqhyy0c7a3y4dlwnn9";
+      }
+    )
+    { }
+, clangSupport ? true
+}:
 
-with python3Packages;
+with pkgs;
 
-let
-  stdenv = if stdenv.hostPlatform.isDarwin then clangStdenv else gccStdenv;
-in
-callPackage ./derivation.nix { }
+python3Packages.callPackage ./derivation.nix {
+  src = ./.;
+  stdenv = if clangSupport then clangStdenv else gccStdenv;
+  mdot = callPackage ../mdot/derivation.nix {
+    src = ./mdot/.;
+    stdenv = if clangSupport then clangStdenv else gccStdenv;
+  };
+}
--- derivation.nix
+++ derivation.nix
@@ -2,66 +2,66 @@
 , buildPythonPackage ? null
 , boost17x
 , cmakeMinimal
+, mdot
 , ninja
 , numpy ? null
 , pybind11 ? null
 , pytest ? null
-, sphinx ? null
-, sphinx_rtd_theme ? null
-, tbb
+, src ? ./.
 }:
 let
-  src = ./.;
   version = "0.0.0";
 in
-{
+#{
 
-  libfhmdot = stdenv.mkDerivation {
-    name = "libfhmdot";
-    inherit src version;
+  # libfhmdot = stdenv.mkDerivation {
+  #   name = "libfhmdot";
+  #   inherit src version;
 
-    nativeBuildInputs = [ cmakeMinimal ninja ];
-    buildInputs = [ boost17x tbb ];
+  #   nativeBuildInputs = [ cmakeMinimal ninja ];
+  #   buildInputs = [ boost17x tbb ];
 
-    cmakeFlags = [
-      "-DCMAKE_BUILD_TYPE=Release"
-      "-DPROJECT_TESTS=On"
-      "-DPROJECT_SANDBOX=OFF"
-      "-DPROJECT_PYTHON=OFF"
-    ];
-    ninjaFlags = [ "-v" ];
-    makeFlags = [ "VERBOSE=1" ];
+  #   cmakeFlags = [
+  #     "-DCMAKE_BUILD_TYPE=Release"
+  #     "-DPROJECT_TESTS=On"
+  #     "-DPROJECT_SANDBOX=OFF"
+  #     "-DPROJECT_PYTHON=OFF"
+  #   ];
+  #   ninjaFlags = [ "-v" ];
+  #   makeFlags = [ "VERBOSE=1" ];
 
-    enableParallelBuilding = true;
-    enableParallelChecking = true;
+  #   enableParallelBuilding = true;
+  #   enableParallelChecking = true;
 
-    installPhase = "ninja install";
-  };
+  #   installPhase = "ninja install";
+  # };
 
-  pyfhmdot = buildPythonPackage {
-    pname = "pyfhmdot";
-    inherit src version;
+  #  pyfhmdot =
 
-    format = "other";
+buildPythonPackage rec {
+  name = "pyfhmdot";
+  inherit src;
 
-    nativeBuildInputs = [ cmakeMinimal ninja ];
-    buildInputs = [ boost17x pybind11 tbb ];
-    propagatedBuildInputs = [ numpy ];
-    checkInputs = [ pytest ];
+  format = "other";
 
-    cmakeFlags = [
-      "-DCMAKE_BUILD_TYPE=Release"
-      "-DPROJECT_TESTS=ON"
-      "-DPROJECT_SANDBOX=OFF"
-      "-DPROJECT_PRECISION_FLOAT32=OFF"
-      "-DPROJECT_PYTHON=ON"
-    ];
-    ninjaFlags = [ "-v" ];
-    makeFlags = [ "VERBOSE=1" ];
+  nativeBuildInputs = [ cmakeMinimal ninja ];
+  buildInputs = [ boost17x pybind11 ];
+  propagatedBuildInputs = [ numpy ];
+  checkInputs = [ pytest ];
 
-    enableParallelBuilding = true;
-    enableParallelChecking = true;
+  cmakeFlags = [
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DPROJECT_TESTS=ON"
+    "-DPROJECT_SANDBOX=OFF"
+    "-DPROJECT_PRECISION_FLOAT32=OFF"
+    "-DPROJECT_PYTHON=ON"
+  ];
+  ninjaFlags = [ "-v" ];
+  makeFlags = [ "VERBOSE=1" ];
 
-    installPhase = "ninja install";
-  };
+  enableParallelBuilding = true;
+  enableParallelChecking = true;
+
+  installPhase = "ninja install";
 }
+#}
