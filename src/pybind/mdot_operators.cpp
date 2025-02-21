d6115831b8e4b2f4f01188f1f89da912d6a1b159
konex5 <fix@konex.dev>
2025-02-21 16:13:20 +0100

    doing some operator stuff

--- src/pybind/mdot_operators.cpp
+++ src/pybind/mdot_operators.cpp
@@ -30,7 +30,7 @@ pydblocs_type py_single_operator_blocs_real(std::string name) {
   std::vector<std::tuple<index_t, index_t>> shapes;
   std::vector<std::vector<double>> vecs;
 
-  if (name == "sh-id") {
+  if (name == "sh_id_u1") {
     nb_blocs = mdot::real_sh_blocs_crtp<mdot::sh_id_u1>::nb_blocs;
     indices = mdot::real_sh_blocs_crtp<mdot::sh_id_u1>::get_indices();
     sizes = mdot::real_sh_blocs_crtp<mdot::sh_id_u1>::get_sizes();
@@ -38,7 +38,7 @@ pydblocs_type py_single_operator_blocs_real(std::string name) {
     auto arrs = mdot::real_sh_blocs_crtp<mdot::sh_id_u1>::get_arrays();
     for (index_t i = 0; i < nb_blocs; i++)
       vecs.push_back(std::vector<double>(arrs[i].begin(), arrs[i].end()));
-  } else if (name == "sh-sm") {
+  } else if (name == "sh_sm_u1") {
     nb_blocs = mdot::real_sh_blocs_crtp<mdot::sh_sm_u1>::nb_blocs;
     indices = mdot::real_sh_blocs_crtp<mdot::sh_sm_u1>::get_indices();
     sizes = mdot::real_sh_blocs_crtp<mdot::sh_sm_u1>::get_sizes();
@@ -46,7 +46,7 @@ pydblocs_type py_single_operator_blocs_real(std::string name) {
     auto arrs = mdot::real_sh_blocs_crtp<mdot::sh_sm_u1>::get_arrays();
     for (index_t i = 0; i < nb_blocs; i++)
       vecs.push_back(std::vector<double>(arrs[i].begin(), arrs[i].end()));
-  } else if (name == "sh-sp") {
+  } else if (name == "sh_sp_u1") {
     nb_blocs = mdot::real_sh_blocs_crtp<mdot::sh_sp_u1>::nb_blocs;
     indices = mdot::real_sh_blocs_crtp<mdot::sh_sp_u1>::get_indices();
     sizes = mdot::real_sh_blocs_crtp<mdot::sh_sp_u1>::get_sizes();
@@ -54,7 +54,7 @@ pydblocs_type py_single_operator_blocs_real(std::string name) {
     auto arrs = mdot::real_sh_blocs_crtp<mdot::sh_sp_u1>::get_arrays();
     for (index_t i = 0; i < nb_blocs; i++)
       vecs.push_back(std::vector<double>(arrs[i].begin(), arrs[i].end()));
-  } else if (name == "sh-sx") {
+  } else if (name == "sh_sx_u1") {
     nb_blocs = mdot::real_sh_blocs_crtp<mdot::sh_sx_u1>::nb_blocs;
     indices = mdot::real_sh_blocs_crtp<mdot::sh_sx_u1>::get_indices();
     sizes = mdot::real_sh_blocs_crtp<mdot::sh_sx_u1>::get_sizes();
@@ -62,7 +62,7 @@ pydblocs_type py_single_operator_blocs_real(std::string name) {
     auto arrs = mdot::real_sh_blocs_crtp<mdot::sh_sx_u1>::get_arrays();
     for (index_t i = 0; i < nb_blocs; i++)
       vecs.push_back(std::vector<double>(arrs[i].begin(), arrs[i].end()));
-  } else if (name == "sh-sz") {
+  } else if (name == "sh_sz_u1") {
     nb_blocs = mdot::real_sh_blocs_crtp<mdot::sh_sz_u1>::nb_blocs;
     indices = mdot::real_sh_blocs_crtp<mdot::sh_sz_u1>::get_indices();
     sizes = mdot::real_sh_blocs_crtp<mdot::sh_sz_u1>::get_sizes();
@@ -82,12 +82,15 @@ pydblocs_type py_single_operator_blocs_real(std::string name) {
   pydblocs_type output;
 
   for (index_t i = 0; i < nb_blocs; i++) {
-    auto deallocator = py::capsule(&vecs[i], [](void *ptr) {
-      auto vec_ptr = reinterpret_cast<std::vector<dnum_t> *>(ptr);
-      vec_ptr->clear();
+    std::vector<dnum_t> * vec_ptr = new std::vector<dnum_t>;
+    vec_ptr->swap(vecs[i]);
+    auto deallocator = py::capsule(vec_ptr, [](void *ptr) {
+      auto v_ptr = reinterpret_cast<std::vector<dnum_t> *>(ptr);
+      v_ptr->clear();
+      delete v_ptr;
     });
-    numpy_array<dnum_t> np_out(sizes[i], vecs[i].data(), deallocator);
-    np_out.resize({std::get<0>(shapes[i]), std::get<1>(shapes[i])});
+    py::ssize_t shape[2]{std::get<0>(shapes[i]), std::get<1>(shapes[i])};
+    numpy_array<dnum_t> np_out(shape, vec_ptr->data(), deallocator);
     output[{std::get<0>(indices[i]), std::get<1>(indices[i])}] = np_out;
   }
   return output;
@@ -143,31 +146,31 @@ numpy_array<double> py_single_operator_real(std::string name) {
   std::vector<py::ssize_t> shape;
   std::vector<double> vec;
 
-  if (name == "sh-id") {
+  if (name == "sh_id_no") {
     ndim = mdot::real_sh_operators_crtp<mdot::sh_id_no>::size;
     shape = {mdot::real_sh_operators_crtp<mdot::sh_id_no>::shape[0],
              mdot::real_sh_operators_crtp<mdot::sh_id_no>::shape[1]};
     auto arr = mdot::real_sh_operators_crtp<mdot::sh_id_no>::array;
     vec = std::vector<double>(arr.begin(), arr.end());
-  } else if (name == "sh-sm") {
+  } else if (name == "sh_sm_no") {
     ndim = mdot::real_sh_operators_crtp<mdot::sh_sm_no>::size;
     shape = {mdot::real_sh_operators_crtp<mdot::sh_sm_no>::shape[0],
              mdot::real_sh_operators_crtp<mdot::sh_sm_no>::shape[1]};
     auto arr = mdot::real_sh_operators_crtp<mdot::sh_sm_no>::array;
     vec = std::vector<double>(arr.begin(), arr.end());
-  } else if (name == "sh-sp") {
+  } else if (name == "sh_sp_no") {
     ndim = mdot::real_sh_operators_crtp<mdot::sh_sp_no>::size;
     shape = {mdot::real_sh_operators_crtp<mdot::sh_sp_no>::shape[0],
              mdot::real_sh_operators_crtp<mdot::sh_sp_no>::shape[1]};
     auto arr = mdot::real_sh_operators_crtp<mdot::sh_sp_no>::array;
     vec = std::vector<double>(arr.begin(), arr.end());
-  } else if (name == "sh-sx") {
+  } else if (name == "sh_sx_no") {
     ndim = mdot::real_sh_operators_crtp<mdot::sh_sx_no>::size;
     shape = {mdot::real_sh_operators_crtp<mdot::sh_sx_no>::shape[0],
              mdot::real_sh_operators_crtp<mdot::sh_sx_no>::shape[1]};
     auto arr = mdot::real_sh_operators_crtp<mdot::sh_sx_no>::array;
     vec = std::vector<double>(arr.begin(), arr.end());
-  } else if (name == "sh-sz") {
+  } else if (name == "sh_sz_no") {
     ndim = mdot::real_sh_operators_crtp<mdot::sh_sz_no>::size;
     shape = {mdot::real_sh_operators_crtp<mdot::sh_sz_no>::shape[0],
              mdot::real_sh_operators_crtp<mdot::sh_sz_no>::shape[1]};
