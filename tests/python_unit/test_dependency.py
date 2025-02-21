bc20ba17cb777950cff11107d3485ac174c71d63
konex5 <fix@konex.dev>
2025-02-21 16:13:20 +0100

    add dependency

--- tests/python_unit/test_dependency.py
+++ tests/python_unit/test_dependency.py
@@ -2,11 +2,11 @@ import pytest
 
 
 def test_mdot_operators_dep():
-    from mdot_operators import single_operator_real, single_operator_blocs_real
-
-    _ = single_operator_real()
-    _ = single_operator_blocs_real()
+    import mdot_operators
 
 
 def test_mdot_routine_dep():
-    from mdot_routine import mm_to_theta_no_gate
+    import mdot_routine
+
+def test_pyfhmdot_dep():
+    import pyfhmdot
