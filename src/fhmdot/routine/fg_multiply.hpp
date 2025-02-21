2097b097c6f7fcf918dc04ef8d020026728939be
konex5 <fix@konex.dev>
2025-02-21 16:13:19 +0100

    fg_multiply - dummy lines of code

--- src/fhmdot/routine/fg_multiply.hpp
+++ src/fhmdot/routine/fg_multiply.hpp
@@ -1,12 +1,13 @@
 #ifndef FG_MULTIPLY
 #define FG_MULTIPLY
 
+// #include <oneapi/mkl/lapack.hpp>
 #include <vector>
 #include "tbb/tbb.h"
-#include "oneapi/mkl.hpp"
+// #include "oneapi/mkl.hpp"
 
 void multiply_nondeg(int lmat, int rmat) {
-    
+    //oneapi::mkl::lapack::gesvd()
 }
 
 void multiply_deg(std::vector<int> lmat, int rmat, bool isLeft) {
