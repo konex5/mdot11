eff0752c9029c55c884daab4e305590a8b31fd25
konex5 <fix@konex.dev>
2025-02-21 16:13:20 +0100

    rm dummy lines

--- tests/python_unit/conftest.py
+++ tests/python_unit/conftest.py
@@ -2,24 +2,6 @@ import pytest
 
 
 @pytest.fixture
-def make_list_operator_name():
-    def _make_list_operator_name():
-        return ["sh-id", "sh-sp", "sh-sm", "sh-sx", "sh-sz"], ["sh-id-cplx", "sh-sy"]
-
-    return _make_list_operator_name
-
-
-@pytest.fixture
-def make_list_real_operator_name(make_list_operator_name):
-    return make_list_operator_name()[0]
-
-
-@pytest.fixture
-def make_list_cplx_operator_name(make_list_operator_name):
-    return make_list_operator_name()[1]
-
-
-@pytest.fixture
 def make_gate():
     from numpy import array
 
